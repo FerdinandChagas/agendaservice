be8809d1ebdc39d5ccfb9ae583b6e7a01c19f0cd
Ferdinandy Chagas <ferdinandsc@yahoo.com.br>
2024-02-01 19:50:41 -0300

    update schedule

--- assets/js/agendamentos/cadastro.js
+++ assets/js/agendamentos/cadastro.js
@@ -1,3 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let token = localStorage.getItem('token'); // Obtém o token do armazenamento local
+
+    if (token) {
+        // Fazendo uma solicitação GET para a API de categorias
+        fetch('http://localhost:8000/api/agendamentos/servicos/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ' + token
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const dadosTbody = document.getElementById("lista_servicos");
+            // Verifica se o elemento com ID 'dados' existe
+            if (dadosTbody) {
+                // Itera sobre a lista de categorias e insere cada categoria na tabela
+                data.forEach(servico => {
+                    const newOption = document.createElement("option");
+                    newOption.value = servico.name;
+                    newOption.text = servico.name;
+                    newOption.id = servico.id;
+                    dadosTbody.appendChild(newOption);
+                });
+            } else {
+                console.error("Elemento com ID 'dados' não encontrado no DOM.");
+            }
+        })
+        .catch(error => {
+            console.error('Houve um problema com a sua solicitação:', error);
+        });
+    }
+});
+
 document.getElementById('cad_agendamento').addEventListener('submit', function(event) {
     event.preventDefault();
 
